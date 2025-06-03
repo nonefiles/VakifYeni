090df82d3d1fae398ecf417f9506ec1fd552f8f7
nonefiles <101338358+nonefiles@users.noreply.github.com>
2025-06-03 09:51:33 +0300

    Update page.tsx

--- app/hakkimizda/vakif-tuzugu/page.tsx
+++ app/hakkimizda/vakif-tuzugu/page.tsx
@@ -30,7 +30,7 @@ export default function VakifTuzuguPage() {
 
             <div className="flex justify-center">
               <Button asChild className="flex items-center gap-2">
-                <Link href="public/images/vakif-tuzugu.pdf">
+                <Link href="/images/vakif-tuzugu.pdf" target="_blank">
                   <Download className="h-5 w-5" />
                   <span>Vakıf Tüzüğünü İndir (PDF)</span>
                 </Link>
