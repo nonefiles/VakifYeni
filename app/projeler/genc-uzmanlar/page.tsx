dc3c3d51776b28bcf23f456cd8a8f7cbc736d7b5
nonefiles <101338358+nonefiles@users.noreply.github.com>
2025-06-16 10:19:21 +0300

    Update page.tsx

--- app/projeler/genc-uzmanlar/page.tsx
+++ app/projeler/genc-uzmanlar/page.tsx
@@ -195,7 +195,8 @@ const CocugunYolArkadasi: React.FC = () => {
           <div className="max-w-4xl mx-auto text-center">
           <p className="text-gray-700 leading-relaxed text-lg mb-6 text-center">
             "Çocuğun Yol Arkadaşı" çocuk ve gençlere evde destek vermek için özel olarak eğitilmiş,
-            <strong className="text-blue-700"> Psikoloji, Psikolojik Danışmanlık ve Rehberlik, Sosyal Hizmet ve Çocuk Gelişimi gibi alanlarda eğitim gören son sınıf öğrencileri veya mezunlardan oluşur.</strong>
+            <strong className="text-blue-700"> Psikoloji, Psikolojik Danışmanlık ve Rehberlik, Sosyal Hizmet ve Çocuk Gelişimi gibi alanlarda</strong>
+            eğitim gören son sınıf öğrencileri veya mezunlardan oluşur.
           </p>
 
             <div className="grid gap-6 md:grid-cols-2 lg:grid-cols-3">
