16dbfcdb356f224865c5001ffe2ea38870b5a5d6
nonefiles <101338358+nonefiles@users.noreply.github.com>
2025-08-11 11:20:54 +0300

    Update page.tsx

--- app/basvuru/psikoterapi/page.tsx
+++ app/basvuru/psikoterapi/page.tsx
@@ -164,10 +164,10 @@ export default function PsikoterapiBasvuruPage() {
               <p className="text-lg font-bold font-aristotelica mt-4">
                 Telefon Numaraları:
               </p>
-              <p className="text-lg font-aristotelica">
+              <p className="text-lg text-center font-aristotelica">
                 +90 539 889 3375
               </p>
-              <p className="text-lg font-aristotelica">
+              <p className="text-lg text-center font-aristotelica">
                 +90 532 419 3375
               </p>
             </CardContent>
